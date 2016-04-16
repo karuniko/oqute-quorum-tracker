408dcaeaa10ec468e6b80cd8ddab7a989b938f45
karuniko <pittavatakapha@gmail.com>
2016-04-16 23:13:29 +0300

    changed quartz scheduler to spring scheduler

--- backend/src/main/java/com/oqute/jobs/SpringScheduledJob.java
+++ backend/src/main/java/com/oqute/jobs/SpringScheduledJob.java
@@ -1,8 +1,11 @@
 package com.oqute.jobs;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.oqute.service.OQuteApplicationService;
 
 /**
  * Stub of repeatable job
@@ -13,9 +16,12 @@ import org.springframework.stereotype.Component;
 public class SpringScheduledJob {
 
     private final static Logger logger = Logger.getLogger(SpringScheduledJob.class);
+    
+    @Autowired
+    OQuteApplicationService service;
 
     @Scheduled(fixedDelay = 5000)
     public void doJob(){
-        logger.info("I do something useful every 5 sec.");
+        logger.info("I do something useful every 5 sec. " + service.getPerson().getName());
     }
 }
