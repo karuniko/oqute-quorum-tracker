0e47e39664989991cc9ad477311a574fb31cd3b9
karuniko <pittavatakapha@gmail.com>
2016-04-20 17:56:46 +0300

    added caching

--- backend/src/main/java/com/oqute/repository/OQuteApplicationRepository.java
+++ backend/src/main/java/com/oqute/repository/OQuteApplicationRepository.java
@@ -1,12 +1,14 @@
 package com.oqute.repository;
 
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.Repository;
 import com.oqute.domain.Person;
 
 /**
  * Created by karuniko on 09.04.16.
  */
+@Cacheable("entities")
 public interface OQuteApplicationRepository extends Repository<Person, Long> {
     public Person getById(Long id);
     public Person getByName(String name);
